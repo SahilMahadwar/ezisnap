93a46f4e78ffd71545fe5e43ac6081dd4bb942e1
Sahil Mahadwar <accounts@mahadwar.com>
2024-01-23 00:17:04 +0530

    feat: use env port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const globalPrefix = "api";
+
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
 bootstrap();
